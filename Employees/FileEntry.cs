cf379c7ce2818e1ee6a6795cc07496ecf50c385e
milknrice <milknrice@users.noreply.github.com>
2021-02-15 17:45:37 +0200

    Read from file into a string. Split the string and fill up the data for each line into a FileEntry object. Add each FIleEntry object into a list.

---
+++ Employees/FileEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    public class FileEntry
+    {
+        public int EmpID { get; set; }
+
+        public int ProjectID { get; set; }
+        
+        ///yyyy-mm-dd
+        public DateTime DateFrom { get; set; }
+
+        //yyyy-mm-dd
+        public DateTime DateTo { get; set; }
+
+        public void AddEntry (string line)
+        {
+            string[] subs = line.Split(',');
+
+            foreach (var element in subs)
+            {
+                EmpID = Convert.ToInt32(subs[0]);
+                ProjectID = Convert.ToInt32(subs[1]);
+                DateFrom = Convert.ToDateTime(subs[2]);
+                DateTo = Convert.ToDateTime(subs[3]);
+            }
+        }
+    }
+
+    
+}
--- Employees/Program.cs
+++ Employees/Program.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace Employees
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<FileEntry> employees = new List<FileEntry>();
+
+            string dir = Directory.GetCurrentDirectory();
+
+            string[] lines = System.IO.File.ReadAllLines($@"{dir}\data.txt");
+            
+            foreach (var line in lines)
+            {
+                FileEntry entry = new FileEntry();
+                entry.AddEntry(line);
+
+                employees.Add(entry);
+            }
+
+            foreach (var item in employees)
+            {
+                Console.WriteLine($"EmployeeID: {item.EmpID}");
+                Console.WriteLine($"ProjectID: {item.ProjectID}");
+                Console.WriteLine($"DateFrom: {item.DateFrom}");
+                Console.WriteLine($"DateTo: {item.DateTo} \n");
+            }
         }
     }
 }
